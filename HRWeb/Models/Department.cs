cadde81d9c3ed19313a5d5836c8e4d9cbd7e5f12
John Padua <lexter.padua17@gmail.com>
2023-04-17 14:01:44 +0800

    add validation in departments

--- HRWeb/Models/Department.cs
+++ HRWeb/Models/Department.cs
@@ -1,8 +1,11 @@
-﻿namespace HRWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HRWeb.Models
 {
     public class Department
     {
         public int Id { get; set; }
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$", ErrorMessage = "Invalid department format. Only alphabets, spaces, hyphens, and apostrophes are allowed.")]
         public string Name { get; set; }
         public List<Employee>? Employees { get; set; }
 
